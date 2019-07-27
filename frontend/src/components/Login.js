83f87dd07772af1eebcab6e6b8cc2b5429045214
jaakko <jaakko.kuurne@gmail.com>
2019-07-27 13:06:35 +0300

    Create wrapper for Formik.

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 
 export class Login extends Component {
   constructor(props) {
@@ -60,7 +61,71 @@ export class Login extends Component {
             <input type="submit" className="btn btn-primary" value="Login" />
           </div>
         </form>
+        <FormApi />
       </div>
     );
   }
+}
+
+function FormApi(props) {
+  const fields = [
+    {
+      name: "email",
+      type: "email",
+      initial: "Type your email",
+      required: "yes",
+      error: "Invalid email address",
+      callback: alert
+    },
+    {
+      name: "password",
+      type: "password",
+      initial: "",
+      required: "yes",
+      callback: alert
+    }
+  ];
+
+  const initialValues = fields.reduce(function(accumulator, currentValue) {
+    const fieldname = currentValue.name;
+    accumulator[fieldname] = currentValue.initial;
+    return accumulator;
+  }, {});
+
+
+  console.log(initialValues);
+  return (
+    <Formik
+      initialValues={initialValues}
+      validate={values => {
+        let errors = {};
+        if (!values.email) {
+          errors.email = "Required";
+        } else if (
+          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+        ) {
+          errors.email = "Invalid email address";
+        }
+        return errors;
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      {({ isSubmitting }) => (
+        <Form>
+          <Field type="email" name="email" />
+          <ErrorMessage name="email" component="div" />
+          <Field type="password" name="password" />
+          <ErrorMessage name="password" component="div" />
+          <button type="submit" disabled={isSubmitting}>
+            Submit
+          </button>
+        </Form>
+      )}
+    </Formik>
+  );
 }
